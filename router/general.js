df16e0f9a4333172fa44ded80f08a2b2697a0523
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2024-03-18 17:59:23 +0630

    Get book details based on ISBN  using Promise callbacks or async-await with Axios is covered

--- router/general.js
+++ router/general.js
@@ -64,6 +64,20 @@ public_users.get('/isbn/:isbn', function (req, res) {
   }
 });
 
+// Get book details based on ISBN  using Promise callbacks or async-await with Axios is covered
+public_users.get("/books/isbn/:isbn", async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    const data = response.data;
+    const book = JSON.parse(data.book);
+    return res.status(200).json({ book: JSON.stringify(book) });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+})
+
+
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   try {
